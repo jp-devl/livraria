4b3c0dce7fa5ab7fc79b902a18aa431e8eeec852
João Pedro <joaopedro@example.com>
2026-09-15 21:54:51 -0300

    Sincronizacao automatica 2026-09-15 21:54:51

--- app/main.py
+++ app/main.py
@@ -1,12 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from app.schemas.livro import LivroSchema
-from app.routers.livros import livros
-
+from app.routers.livros import router
 
 from app.database.connection import engine
 from app.database.models import Base
 
-Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine) 
 app = FastAPI()
 
 app.include_router(livros.router)
