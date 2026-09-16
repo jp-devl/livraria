fdd9529efde37ca77064fffeee275210a9caff79
João Pedro <joaopedro@example.com>
2026-09-15 21:37:50 -0300

    Sincronizacao automatica 2026-09-15 21:37:50

--- app/main.py
+++ app/main.py
@@ -3,6 +3,9 @@ from app.schemas.livro import LivroSchema
 from app.routers.livros import livros
 
 
+from app.database.connection import engine
+from app.database.models import Base
+
 app = FastAPI()
 
 app.include_router(livros.router)
