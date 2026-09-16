de50d03ecd7a1dd4c09d7e8bba6330b9a3c1ec6b
João Pedro <joaopedro@example.com>
2026-09-15 21:32:05 -0300

    Sincronizacao automatica 2026-09-15 21:32:05

---
+++ app/database/models.py
@@ -0,0 +1,12 @@
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Livro(Base):
+    __tablename__ = "livros"
+
+    id = Column(Integer, primary_key=True)
+    titulo = Column(String(100))
+    autor = Column(String(100))
+    ano_publicacao = Column(Integer)
